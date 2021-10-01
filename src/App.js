bd8f80505bde366a68415f1cb64d6790f194fbef
Nathan Stern <sternn@purdue.edu>
2021-10-01 15:46:56 -0400

    added temporary homepage for hosting

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import '@popperjs/core';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { useState } from 'react';
+
+import Modal from 'react-bootstrap/Modal';
 
 function App() {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleShow = () => setShowModal(true);
+  const handleClose = () => setShowModal(false);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn How to Be Awesome
-        </a>
+
+        <p>Future Homepage of NathanStern.github.io</p>
+
+        <button className="btn btn-primary" onClick={handleShow}>Learn More</button>
+
+        <Modal show={showModal} onHide={handleClose}>
+          <Modal.Header>
+            <Modal.Title>Info</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            This website is the future home of an interactive resume and project list. For now, please take a look at <a href="https://linkedin.com/in/nathan-stern">my LinkedIn</a>.
+          </Modal.Body>
+          <Modal.Footer>
+            <button className="btn btn-secondary" onClick={handleClose}>Close</button>
+          </Modal.Footer>
+        </Modal>
+
       </header>
     </div>
   );
